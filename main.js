8a023a3d50fe37add705488d4e21e36149e22520
Nicole Lemos <niikii.ense@gmail.com>
2022-07-05 14:13:04 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let password = prompt('Ingresá la contaseña')
+
+while(password != 'stan stray kids'){
+    alert('La contraseña "' + password + '" es incorrecta')
+    password = prompt('Intentá otra vez')
+}
